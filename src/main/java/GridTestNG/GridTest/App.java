d04c6d8e8fa308170c6340c3829761b3eb59b851
Prithviraj Gangadharaiah <Prithviraj Gangadharaiah>
2016-12-22 06:33:07 -0500

    Initial Commit

---
+++ src/main/java/GridTestNG/GridTest/App.java
@@ -0,0 +1,13 @@
+package GridTestNG.GridTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GridTestNG/GridTest/IntegrationTest/SampleTest.java
@@ -0,0 +1,11 @@
+package GridTestNG.GridTest.IntegrationTest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Maven ran successfully! Don't worry God will help you!");
+  }
+}
